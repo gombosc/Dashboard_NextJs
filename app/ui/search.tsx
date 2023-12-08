f1f4dbb354eef0a0b3e4bc71609ee32d1446a5d5
Cosmin <cosmingombos@gmail.com>
2023-12-08 23:28:27 +0100

    ...

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,14 +2,17 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const {replace} = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
    if (term) {
       params.set('query', term);
     } else {
@@ -17,13 +20,15 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     replace(`${pathname}?${params.toString()}`);
     console.log(params, pathname)
-  }
+  },300)
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
-      <input onChange={ (e) => handleSearch(e.target.value) }
+      <input onChange={ (e) => {handleSearch(e.target.value) }}
+        defaultValue={searchParams.get('query')?.toString()}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
